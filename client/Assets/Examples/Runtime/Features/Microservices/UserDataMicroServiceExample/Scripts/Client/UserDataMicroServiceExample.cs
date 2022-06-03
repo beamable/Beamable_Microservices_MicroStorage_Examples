84665a534f0f3164fe87f473b1e9cd223134a2e0
NicoleHashbang <95370658+NicoleHashbang@users.noreply.github.com>
2022-06-03 12:08:26 -0700

    [DOCS-901] Microservice Microstorage Update (#2)
    * Update to Beamable 1.2.1
    
    * Update to BeamContext

--- client/Assets/Examples/Runtime/Features/Microservices/UserDataMicroServiceExample/Scripts/Client/UserDataMicroServiceExample.cs
+++ client/Assets/Examples/Runtime/Features/Microservices/UserDataMicroServiceExample/Scripts/Client/UserDataMicroServiceExample.cs
@@ -31,9 +31,10 @@ namespace Beamable.Examples.Features.Microservices.UserDataMicroServiceExample
         //  Methods  --------------------------------------
         private async void SetupBeamable()
         {
-            var beamableAPI = await Beamable.API.Instance;
+            var beamContext = BeamContext.Default;
+            await beamContext.OnReady;
 
-            Debug.Log($"beamableAPI.User.id = {beamableAPI.User.id}");
+            Debug.Log($"beamContext.PlayerId = {beamContext.PlayerId}");
             
             _userDataServiceClient = new UserDataServiceClient();
             
